3819879ed2d0a3db266321cb18cf8e1ebab2a7ca
Pedro Ribeiro <102664733+Ribs2004@users.noreply.github.com>
2023-11-12 14:48:41 -0300

    Initialize shadcn/ui

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div>
+      <Button className="text-xs" size="sm">Hello, world!</Button>
+    </div>
+  );
+};
+
+export default Home;
